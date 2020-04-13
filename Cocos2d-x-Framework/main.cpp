d53217c910e23e05f22a5c45dbee1e3753c1eec3
wang.ning <wang.ning_1920@immomo.com>
2020-04-13 19:36:55 +0800

    Initial Commit

---
+++ Cocos2d-x-Framework/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cocos2d-x-Framework
+//
+//  Created by David on 2020/4/13.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
